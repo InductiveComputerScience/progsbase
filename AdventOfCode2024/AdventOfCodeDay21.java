33b714069768c8e4877dcc4cc0b5ad9111618409
Martin F. Johansen <martinfjohansen@inductive.no>
2025-01-02 20:39:40 +0100

    Day 21

--- AdventOfCode2024/AdventOfCodeDay21.java
+++ AdventOfCode2024/AdventOfCodeDay21.java
@@ -1,10 +1,16 @@
 package AdventOfCode.AdventOfCode;
 
+import Trees.RedBlackTrees.RedBlackNode;
+import Trees.RedBlackTrees.RedBlackTree;
+import lists.DynamicArrayCharacters.Structures.DynamicArrayCharacters;
 import lists.LinkedListStrings.Structures.LinkedListStrings;
 import references.references.*;
 
+import static DataStructures.Array.Structures.Structures.CreateNumberData;
+import static Trees.RedBlackTrees.RedBlackTrees.*;
 import static aarrays.arrays.arrays.aCopyString;
 import static java.lang.Math.min;
+import static lists.DynamicArrayCharacters.DynamicArrayCharactersFunctions.DynamicArrayCharactersFunctions.*;
 import static lists.LinkedListStrings.LinkedListStringsFunctions.LinkedListStringsFunctions.*;
 import static numbers.NumberToString.NumberToString.CreateStringDecimalFromNumber;
 import static numbers.StringToNumber.StringToNumber.CreateNumberFromDecimalString;
@@ -126,24 +132,40 @@ public class AdventOfCodeDay21 {
         }
 
         ll = CreateLinkedListString();
-        AllCombinations(ll, "".toCharArray(), lol, 0d);
+        DynamicArrayCharacters buffer = CreateDynamicArrayCharacters();
+        AllCombinations(ll, buffer, lol, 0d);
 
         shortest = LinkedListStringsToArray(ll);
 
         return shortest;
     }
 
-    public static void AllCombinations(LinkedListStrings ll, char[] str, StringArrayReference[] lol, double p) {
-        double i;
-        char [] newstr;
+    public static void AllCombinations(LinkedListStrings ll, DynamicArrayCharacters buffer, StringArrayReference[] lol, double p) {
+        double i, oldlen;
+        char [] str;
 
         if(p == lol.length){
+            str = DynamicArrayCharactersToArray(buffer);
             LinkedListAddString(ll, str);
         }else {
             for (i = 0d; i < lol[(int) p].stringArray.length; i = i + 1d) {
-                newstr = ConcatenateString(str, lol[(int) p].stringArray[(int) i].string);
-                AllCombinations(ll, newstr, lol, p + 1d);
+                oldlen = DynamicArrayCharactersLength(buffer);
+                DynamicArrayAddString(buffer, lol[(int) p].stringArray[(int) i].string);
+
+                AllCombinations(ll, buffer, lol, p + 1d);
+
+                for(; DynamicArrayCharactersLength(buffer) > oldlen;){
+                    DynamicArrayRemoveCharacter(buffer, DynamicArrayCharactersLength(buffer) - 1d);
+                }
             }
+        }
+    }
+
+    public static void DynamicArrayAddString(DynamicArrayCharacters da, char [] str){
+        double i;
+
+        for(i = 0d; i < str.length; i = i + 1d){
+            DynamicArrayAddCharacter(da, str[(int)i]);
         }
     }
 
@@ -204,6 +226,213 @@ public class AdventOfCodeDay21 {
         }
     }
 
+    public static char[] ComputeDay21Part2(char[] input, double robots) {
+        double n, i, j, numstartx, numstarty, minlen, candidateLength;
+        char [] output, code, roboCode;
+        StringReference [] numField, codes, firstRobotPresses;
+        boolean minlenSet;
+        RedBlackTree cache;
+
+        // Read codes
+        codes = SplitByCharacter(input, '\n');
+
+        // Create fields
+        numField = new StringReference[4];
+        numField[0] = CreateStringReference("789".toCharArray());
+        numField[1] = CreateStringReference("456".toCharArray());
+        numField[2] = CreateStringReference("123".toCharArray());
+        numField[3] = CreateStringReference(" 0A".toCharArray());
+
+        numstartx = 2d;
+        numstarty = 3d;
+
+        n = 0d;
+
+        for(i = 0d; i < codes.length; i = i + 1d){
+            code = codes[(int) i].string;
+
+            // Keypad
+            firstRobotPresses = FindPresses(numField, code, numstartx, numstarty);
+
+            // Robots
+            minlen = 0d;
+            minlenSet = false;
+            cache = CreateRedBlackTree();
+            for(j = 0d; j < firstRobotPresses.length; j = j + 1d) {
+                roboCode = firstRobotPresses[(int)j].string;
+
+                candidateLength = MoveRobot(roboCode, 0d, robots, cache);
+
+                if(!minlenSet){
+                    minlen = candidateLength;
+                    minlenSet = true;
+                }else {
+                    minlen = min(minlen, candidateLength);
+                }
+            }
+
+            code = ReplaceString(code, "A".toCharArray(), "".toCharArray());
+            n = n + minlen * CreateNumberFromDecimalString(code);
+        }
+
+        // Produce output
+        output = CreateStringDecimalFromNumber(n);
+
+        return output;
+    }
+
+    public static double MoveRobot(char [] moves, double level, double goal, RedBlackTree cache){
+        double i, j, len, minlen, partlen;
+        char from, to;
+        StringReference [] combos;
+        char [] combo, key;
+        boolean minlenSet;
+        BooleanReference foundRef;
+        RedBlackNode node;
+
+        foundRef = new BooleanReference();
+
+        if(level < goal) {
+            from = 'A';
+
+            len = 0d;
+
+            for (i = 0d; i < moves.length; i = i + 1d) {
+                to = moves[(int) i];
+
+                combos = GetMoveCombos(from, to);
+
+                minlen = 0d;
+                minlenSet = false;
+                for (j = 0d; j < combos.length; j = j + 1d) {
+                    combo = combos[(int)j].string;
+
+                    key = CreateStringDecimalFromNumber(level);
+                    key = AppendCharacter(key, '-');
+                    key = AppendString(key, combo);
+
+                    node = Search(cache, key, foundRef);
+
+                    if(!foundRef.booleanValue) {
+                        partlen = MoveRobot(combo, level + 1d, goal, cache);
+
+                        InsertData(cache, key, CreateNumberData(partlen));
+                    }else{
+                        partlen = node.value.number;
+                    }
+
+                    if(!minlenSet) {
+                        minlen = partlen;
+                        minlenSet = true;
+                    }else{
+                        minlen = min(minlen, partlen);
+                    }
+                }
+
+                len = len + minlen;
+
+                from = to;
+            }
+        }else{
+            len = moves.length;
+        }
+
+        return len;
+    }
+
+    public static StringReference [] GetMoveCombos(char from, char to){
+        Move [] moves;
+        double i;
+        StringReference [] r;
+
+        moves = GetMoves();
+        r = new StringReference[0];
+
+        for(i = 0d; i < moves.length; i = i + 1d){
+            Move move = moves[(int)i];
+
+            if(move.from == from && move.to == to){
+                r = move.moves;
+            }
+        }
+
+        return r;
+    }
+
+    public static Move [] GetMoves(){
+        Move [] moves = new Move[25];
+
+        moves[0] = CreateSingleMove('^', 'A', ">A".toCharArray());
+        moves[1] = CreateSingleMove('^', '<', "v<A".toCharArray());
+        moves[2] = CreateSingleMove('^', 'v', "vA".toCharArray());
+        moves[3] = CreateDoubleMove('^', '>', ">vA".toCharArray(), "v>A".toCharArray());
+
+        moves[4] = CreateSingleMove('A', '^', "<A".toCharArray());
+        moves[5] = CreateDoubleMove('A', '<', "<v<A".toCharArray(), "v<<A".toCharArray());
+        moves[6] = CreateDoubleMove('A', 'v', "<vA".toCharArray(), "v<A".toCharArray());
+        moves[7] = CreateSingleMove('A', '>', "vA".toCharArray());
+
+        moves[8] = CreateSingleMove('<', '^', ">^A".toCharArray());
+        moves[9] = CreateDoubleMove('<', 'A', ">>^A".toCharArray(), ">^>A".toCharArray());
+        moves[10] = CreateSingleMove('<', '>', ">>A".toCharArray());
+        moves[11] = CreateSingleMove('<', 'v', ">A".toCharArray());
+
+        moves[12] = CreateSingleMove('v', '^', "^A".toCharArray());
+        moves[13] = CreateDoubleMove('v', 'A', "^>A".toCharArray(), ">^A".toCharArray());
+        moves[14] = CreateSingleMove('v', '<', "<A".toCharArray());
+        moves[15] = CreateSingleMove('v', '>', ">A".toCharArray());
+
+        moves[16] = CreateDoubleMove('>', '^', "^<A".toCharArray(), "<^A".toCharArray());
+        moves[17] = CreateSingleMove('>', 'A', "^A".toCharArray());
+        moves[18] = CreateSingleMove('>', '<', "<<A".toCharArray());
+        moves[19] = CreateSingleMove('>', 'v', "<A".toCharArray());
+
+        moves[20] = CreateSingleMove('^', '^', "A".toCharArray());
+        moves[21] = CreateSingleMove('A', 'A', "A".toCharArray());
+        moves[22] = CreateSingleMove('<', '<', "A".toCharArray());
+        moves[23] = CreateSingleMove('v', 'v', "A".toCharArray());
+        moves[24] = CreateSingleMove('>', '>', "A".toCharArray());
+
+        return moves;
+    }
+
+    static class Move{
+        public char from;
+        public char to;
+        StringReference [] moves;
+    }
+
+    public static Move CreateSingleMove(char from, char to, char[] seq) {
+        Move move;
+
+        move = new Move();
+
+        move.from = from;
+        move.to = to;
+        move.moves = new StringReference[1];
+        move.moves[0] = new StringReference();
+        move.moves[0].string = seq;
+
+        return move;
+    }
+
+    public static Move CreateDoubleMove(char from, char to, char[] seq1, char [] seq2) {
+        Move move;
+
+        move = new Move();
+
+        move.from = from;
+        move.to = to;
+        move.moves = new StringReference[2];
+        move.moves[0] = new StringReference();
+        move.moves[0].string = seq1;
+        move.moves[1] = new StringReference();
+        move.moves[1].string = seq2;
+
+        return move;
+    }
+
+    // Lib
     public static int GetFieldHeight(StringReference[] field) {
         return field.length;
     }
