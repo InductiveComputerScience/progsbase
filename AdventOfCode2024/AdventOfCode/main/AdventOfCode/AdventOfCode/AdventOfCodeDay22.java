0959dcb2acfe9cd449fe9d305e8ad8ff2c51059c
Martin F. Johansen <martinfjohansen@inductive.no>
2025-01-05 21:37:25 +0100

    Day 22

--- AdventOfCode2024/AdventOfCode/main/AdventOfCode/AdventOfCode/AdventOfCodeDay22.java
+++ AdventOfCode2024/AdventOfCode/main/AdventOfCode/AdventOfCode/AdventOfCodeDay22.java
@@ -6,10 +6,12 @@ import Trees.RedBlackTrees.RedBlackTree;
 import references.references.BooleanReference;
 import references.references.StringReference;
 
+import static Bits.Bitwise.Bitwise.XorBytes;
 import static DataStructures.Array.Arrays.Arrays.*;
 import static DataStructures.Array.Structures.Structures.*;
 import static Trees.RedBlackTrees.RedBlackTrees.*;
 import static java.lang.Math.floor;
+import static math.math.math.Truncate;
 import static numbers.NumberToString.NumberToString.CreateStringDecimalFromNumber;
 import static numbers.StringToNumber.StringToNumber.CreateNumberFromDecimalString;
 import static references.references.references.CreateBooleanReference;
@@ -18,7 +20,7 @@ import static strings.strings.strings.*;
 public class AdventOfCodeDay22 {
     public static char[] ComputeDay22Part1(char[] input) {
         double n, i, j, secret;
-        char [] output;
+        char [] output, secretStr;
         StringReference [] secretsStr;
 
         // Read initial secrets
@@ -26,14 +28,11 @@ public class AdventOfCodeDay22 {
 
         n = 0d;
         for(i = 0d; i < secretsStr.length; i = i + 1d){
-            char[] secretStr = secretsStr[(int) i].string;
+            secretStr = secretsStr[(int) i].string;
             secret = CreateNumberFromDecimalString(secretStr);
-
-            //System.out.println(secret);
 
             for(j = 0d; j < 2000; j = j + 1d){
                 secret = Next(secret);
-                //System.out.println(secret);
             }
 
             n = n + secret;
@@ -67,20 +66,21 @@ public class AdventOfCodeDay22 {
     }
 
     public static double Mix(double a, double b) {
-        long A = (long)a;
-        long B = (long)b;
+        double A, B;
 
-        return A ^ B;
+        A = Truncate(a);
+        B = Truncate(b);
+
+        return XorBytes(A, B, 6d);
     }
 
     public static double Prune(double a) {
-
-        return (long)(a % 16777216);
+        return Truncate(a % 16777216d);
     }
 
     public static char[] ComputeDay22Part2(char[] input) {
-        double n, i, j, secret, prev, price, diff;
-        char [] output;
+        double n, i, j, secret, prev, price, diff, len, sum, maximum;
+        char [] output, secretStr, key;
         StringReference [] secretsStr;
         RedBlackTree [] bananas;
         Array seq;
@@ -97,18 +97,14 @@ public class AdventOfCodeDay22 {
 
         bananas = new RedBlackTree [secretsStr.length];
 
-        n = 0d;
         for(i = 0d; i < secretsStr.length; i = i + 1d){
             bananas[(int)i] = CreateRedBlackTree();
             seq = CreateArray();
 
-            char[] secretStr = secretsStr[(int) i].string;
+            secretStr = secretsStr[(int) i].string;
             secret = CreateNumberFromDecimalString(secretStr);
 
-            //System.out.println(secret);
-
             price = floor(secret % 10);
-            //System.out.println((int)secret + ": " + (int)price);
 
             prev = price;
 
@@ -120,11 +116,8 @@ public class AdventOfCodeDay22 {
 
                 AddNumberToArray(seq, diff);
 
-                //System.out.println((int)secret + ": " + (int)price + " ("+(int)diff+")");
-
-                double len = ArrayLength(seq);
+                len = ArrayLength(seq);
                 if(len >= 4) {
-                    char [] key;
                     key = CreateStringDecimalFromNumber(ArrayIndex(seq, len - 4).number);
                     key = AppendCharacter(key, ',');
                     key = AppendString(key, CreateStringDecimalFromNumber(ArrayIndex(seq, len - 3).number));
@@ -133,13 +126,10 @@ public class AdventOfCodeDay22 {
                     key = AppendCharacter(key, ',');
                     key = AppendString(key, CreateStringDecimalFromNumber(ArrayIndex(seq, len - 1).number));
 
-                    //System.out.println(key);
-
                     found = CreateBooleanReference(false);
                     Search(bananas[(int)i], key, found);
                     if(!found.booleanValue){
                         InsertData(bananas[(int)i], key, CreateNumberData(price));
-                        //bananas[(int)i].put(key, price);
                     }
 
                     found = CreateBooleanReference(false);
@@ -152,15 +142,11 @@ public class AdventOfCodeDay22 {
 
                 prev = price;
             }
-
-            System.out.println("finding " + i + "/" + secretsStr.length);
         }
 
-        double sum = 0;
-        double maximum = 0;
+        maximum = 0;
         for(i = 0d; i < ArrayLength(keys); i = i + 1d){
-            System.out.println(i + "/" + ArrayLength(keys));
-            char [] key = ArrayIndex(keys, i).string;
+            key = ArrayIndex(keys, i).string;
             sum = 0;
             // Find key price
             for(j = 0d; j < secretsStr.length; j = j + 1d){
@@ -174,15 +160,9 @@ public class AdventOfCodeDay22 {
             // Check
             if(sum > maximum){
                 maximum = sum;
-
-                //System.out.println("Found better:");
-                //System.out.println(bestkey);
-                //System.out.println(maximum);
             }
         }
 
-        //System.out.println(bestkey);
-        //System.out.println(maximum);
         n = maximum;
 
         // Produce output
@@ -191,34 +171,3 @@ public class AdventOfCodeDay22 {
         return output;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
