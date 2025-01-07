aaef57782ddea2f2997e293a7e2c6cc24e4bcc7e
Martin F. Johansen <martinfjohansen@inductive.no>
2025-01-07 19:16:24 +0100

    Day 23

--- AdventOfCode2024/AdventOfCode/main/AdventOfCode/AdventOfCode/AdventOfCodeDay23.java
+++ AdventOfCode2024/AdventOfCode/main/AdventOfCode/AdventOfCode/AdventOfCodeDay23.java
@@ -2,126 +2,149 @@ package AdventOfCode.AdventOfCode;
 
 import DataStructures.Array.Structures.Array;
 import DataStructures.Array.Structures.Data;
+import QuickSort.QuickSortStrings.QuickSortStrings;
 import Trees.RedBlackTrees.RedBlackNode;
 import Trees.RedBlackTrees.RedBlackTree;
 import references.references.BooleanReference;
+import references.references.StringArrayReference;
 import references.references.StringReference;
 
 import java.util.*;
 
 import static DataStructures.Array.Arrays.Arrays.*;
 import static DataStructures.Array.Structures.Structures.*;
+import static QuickSort.QuickSortStrings.QuickSortStrings.xQuickSortStrings;
 import static Trees.RedBlackTrees.RedBlackTrees.*;
+import static aarrays.arrays.arrays.aCopyString;
+import static aarrays.arrays.arrays.aStringsEqual;
 import static numbers.NumberToString.NumberToString.CreateStringDecimalFromNumber;
-import static references.references.references.CreateBooleanReference;
+import static references.references.references.*;
 import static strings.strings.strings.*;
 
 public class AdventOfCodeDay23 {
     public static char[] ComputeDay23Part1(char[] input) {
-        double n, i, j, secret;
-        char [] output, secretStr;
+        double n, i;
+        char [] output;
         StringReference [] lines;
-        //RedBlackTree computersSet, links;
-        //Array computers, triples;
-        Map<String, List<String>> links;
-        Set<String> computers;
+        RedBlackTree links;
+        StringSet computers, col, triples;
+        StringReference[] cs;
+        char [] c1, c2, com, triple;
+        Array filtered, array;
 
         // Read initial secrets
         lines = SplitByCharacter(input, '\n');
 
-        //computersSet = CreateRedBlackTree();
-        links = new HashMap<>();
-        computers = new HashSet<>();
+        links = CreateRedBlackTree();
+        computers = CreateStringSet();
 
         // Find computers and links
         for(i = 0; i < lines.length; i = i + 1d){
-            StringReference[] cs = SplitByCharacter(lines[(int) i].string, '-');
-            String c1 = new String(cs[0].string);
-            String c2 = new String(cs[1].string);
-            computers.add(c1);
-            computers.add(c2);
+            cs = SplitByCharacter(lines[(int) i].string, '-');
 
-            if(!links.containsKey(c1)){
-                links.put(c1, new ArrayList<>());
-            }
-            links.get(c1).add(c2);
+            c1 = cs[0].string;
+            c2 = cs[1].string;
 
-            if(!links.containsKey(c2)){
-                links.put(c2, new ArrayList<>());
+            AddToSet(computers, c1);
+            AddToSet(computers, c2);
+
+            if(!TreeHasKey(links, c1)){
+                InsertData(links, c1, CreateArrayData());
             }
-            links.get(c2).add(c1);
+            array = GetTreeData(links, c1).array;
+            AddStringToArray(array, c2);
+
+            if(!TreeHasKey(links, c2)){
+                InsertData(links, c2, CreateArrayData());
+            }
+            array = GetTreeData(links, c2).array;
+            AddStringToArray(array, c1);
         }
-
-        //System.out.println(computers);
-        //System.out.println(links);
 
         // Find all triples
-        Set<Set<String>> triples = new HashSet<>();
-        Set<String> col = new HashSet<>();
-        for(String com : computers){
-            col.add(com);
+        triples = CreateStringSet();
+        col = CreateStringSet();
+        for(i = 0; i < ArrayLength(computers.array); i = i + 1){
+            com = ArrayIndex(computers.array, i).string;
+            AddToSet(col, com);
             FindLinks(triples, links, com, com, col, 0);
-            col.remove(com);
+            RemoveFromSet(col, com);
         }
 
-        //System.out.println(triples);
-
         // Filter those with a t-computer
-        Set<String> filtered = new HashSet<>();
-        for(Set<String> triple : triples){
-            if(triple.toString().contains(", t") || triple.toString().contains("[t")){
-                filtered.add(triple.toString());
+        filtered = CreateArray();
+        for(i = 0d; i < ArrayLength(triples.array); i = i + 1d){
+            triple = ArrayIndex(triples.array, i).string;
+            if(ContainsString(triple, ",t".toCharArray()) || StartsWith(triple, "t".toCharArray())){
+                AddStringToArray(filtered, triple);
             }
         }
 
-        //System.out.println(filtered);
-
         // Produce output
-        n = filtered.size();
+        n = ArrayLength(filtered);
         output = CreateStringDecimalFromNumber(n);
 
         return output;
     }
 
-    public static double NameToNum(char [] name){
-        double n;
+    public static void FindLinks(StringSet triples, RedBlackTree links, char [] com, char [] goal, StringSet col, double d) {
+        Array cons;
+        char [] con, str;
+        double i, c;
+        StringArrayReference strArr;
 
-        n = name[0]*100 + name[1];
-
-        return n;
-    }
-
-    public static void FindLinks(Set<Set<String>> triples, Map<String, List<String>> links, String com, String goal, Set<String> col, double d) {
+        cons = GetTreeData(links, com).array;
 
         if(d == 2){
-            List<String> cons = links.get(com);
-            for(String con : cons) {
-                if (con.equals(goal)) {
-                    triples.add(new HashSet<>(col));
+
+            for(c = 0d; c < ArrayLength(cons); c = c + 1) {
+                con = ArrayIndex(cons, c).string;
+                if (aStringsEqual(con, goal)) {
+                    str = "".toCharArray();
+
+                    strArr = ToStaticStringArray(col.array);
+                    xQuickSortStrings(strArr);
+
+                    for(i = 0; i < strArr.stringArray.length; i = i + 1d) {
+                        str = AppendString(str, strArr.stringArray[(int)i].string);
+                        if(i + i < ArrayLength(col.array)) {
+                            str = AppendCharacter(str, ',');
+                        }
+                    }
+
+                    AddToSet(triples, str);
                 }
             }
         }else{
-            List<String> cons = links.get(com);
-            for(String con : cons) {
-                if(!col.contains(con)) {
-                    col.add(con);
+            for(i = 0; i < ArrayLength(cons); i = i + 1d) {
+                con = ArrayIndex(cons, i).string;
+                if(!SetContains(col, con)) {
+                    AddToSet(col, con);
                     FindLinks(triples, links, con, goal, col, d + 1);
-                    col.remove(con);
+                    RemoveFromSet(col, con);
                 }
             }
         }
     }
 
-    public static boolean SetContains(RedBlackTree computersSet, char[] key) {
-        BooleanReference found = new BooleanReference();
-        Search(computersSet, key, found);
-        return found.booleanValue;
+    public static StringArrayReference ToStaticStringArray(Array array) {
+        StringArrayReference arr;
+        double i;
+
+        arr = new StringArrayReference();
+        arr.stringArray = new StringReference[(int)ArrayLength(array)];
+
+        for(i = 0; i < ArrayLength(array); i = i + 1d){
+            arr.stringArray[(int)i] = new StringReference();
+            arr.stringArray[(int)i].string = aCopyString(ArrayIndex(array, i).string);
+        }
+
+        return arr;
     }
 
-
     public static char[] ComputeDay23Part2(char[] input) {
-        double n, i, j, secret;
-        char [] output, secretStr;
+        double i;
+        char [] output;
         StringReference [] lines;
         //RedBlackTree computersSet, links;
         //Array computers, triples;
@@ -134,7 +157,6 @@ public class AdventOfCodeDay23 {
         // Read initial secrets
         lines = SplitByCharacter(input, '\n');
 
-        //computersSet = CreateRedBlackTree();
         links = new HashMap<>();
         computers = new HashSet<>();
 
@@ -236,6 +258,82 @@ public class AdventOfCodeDay23 {
         return output;
     }
 
+    public static class StringSet{
+        public RedBlackTree rbtree;
+        public Array array;
+    }
+
+    public static StringSet CreateStringSet(){
+        StringSet set;
+
+        set = new StringSet();
+        set.rbtree = CreateRedBlackTree();
+        set.array = CreateArray();
+
+        return set;
+    }
+
+    public static void AddToSet(StringSet set, char [] str){
+        BooleanReference foundRef;
+        double p;
+
+        foundRef = CreateBooleanReference(false);
+
+        Search(set.rbtree, str, foundRef);
+
+        if(!foundRef.booleanValue){
+            AddStringToArray(set.array, str);
+            p = ArrayLength(set.array) - 1d;
+            InsertData(set.rbtree, str, CreateNumberData(p));
+        }
+    }
+
+    public static void RemoveFromSet(StringSet set, char [] str){
+        BooleanReference foundRef;
+        RedBlackNode node;
+        double p;
+
+        foundRef = CreateBooleanReference(false);
+
+        node = Search(set.rbtree, str, foundRef);
+
+        if(foundRef.booleanValue){
+            p = node.value.number;
+
+            Remove(set.rbtree, node);
+            ArrayRemove(set.array, p);
+        }
+    }
+
+    public static boolean SetContains(StringSet set, char [] str){
+        BooleanReference foundRef;
+
+        foundRef = CreateBooleanReference(false);
+
+        Search(set.rbtree, str, foundRef);
+
+        return foundRef.booleanValue;
+    }
+
+    public static boolean TreeHasKey(RedBlackTree tree, char [] str){
+        BooleanReference foundRef;
+
+        foundRef = CreateBooleanReference(false);
+
+        Search(tree, str, foundRef);
+
+        return foundRef.booleanValue;
+    }
+
+    public static Data GetTreeData(RedBlackTree tree, char [] str){
+        BooleanReference foundRef;
+
+        foundRef = CreateBooleanReference(false);
+
+        RedBlackNode node = Search(tree, str, foundRef);
+
+        return node.value;
+    }
 }
 
 
